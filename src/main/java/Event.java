e14b206352e38a6c361f3e8b399df233e7c411d4
macarpenter12 <macarpenter12@users.noreply.github.com>
2019-10-10 14:03:18 -0600

    added all files

---
+++ src/main/java/Event.java
@@ -0,0 +1,11 @@
+public class Event {
+    private String eventID;
+    private String username;
+    private String personID;
+    private double latitude;
+    private double longitude;
+    private String country;
+    private String city;
+    private String type;
+    private int year;
+}
---
+++ src/main/java/Person.java
@@ -0,0 +1,10 @@
+public class Person {
+    private String personID;
+    private String username;
+    private String firstName;
+    private String lastName;
+    private String gender;
+    private String fatherID;
+    private String motherID;
+    private String spouseID;
+}
