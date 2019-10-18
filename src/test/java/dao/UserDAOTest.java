ba0d3edf4b0bc69b65cf7b25379cdd7b418abea0
macarpenter12 <macarpenter12@users.noreply.github.com>
2019-10-18 00:43:06 -0600

    EventDAO and EventDAOTest are done. Now must implement PersonDAO and UserDao, along with unit tests.

---
+++ src/test/java/dao/UserDAOTest.java
@@ -0,0 +1,16 @@
+package dao;
+
+import DAO.EventDAO;
+import exception.DataAccessException;
+import familymap.Event;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import server.Server;
+
+import java.sql.Connection;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class UserDAOTest {
+}
