d211f06d4381a5ad584dd95ea86e939021817cd9
mcarpe12 <mcarpe12@byu.edu>
2019-11-22 14:07:14 -0700

    Did not need FileNotExist exception, got rid of it

--- src/main/java/service/FillService.java
+++ src/main/java/service/FillService.java
@@ -10,7 +10,6 @@ import familymap.Person;
 import familymap.User;
 import response.FillResponse;
 
-import java.io.FileNotFoundException;
 import java.util.ArrayList;
 
 /**
@@ -18,105 +17,97 @@ import java.util.ArrayList;
  */
 public class FillService {
 
-	/**
-	 * @param userName       Useranme associated with the person to generate data for.
-	 * @param numGenerations The number of generations of ancestors to create.
-	 * @return Returns the response as a serializable object.
-	 * @throws DataAccessException If an error occurred when accessing the database.
-	 */
-	public FillResponse fill(String userName, int numGenerations) throws DataAccessException {
-		Database db = new Database();
-		int personsAdded = 0;
-		int eventsAdded = 0;
-		try {
-			// Delete all existing events associated with userName
-			EventDAO eDao = new EventDAO(db.openConnection());
-			eDao.deleteByUser(userName);
-			db.closeConnection(true);
+    /**
+     * @param userName       Useranme associated with the person to generate data for.
+     * @param numGenerations The number of generations of ancestors to create.
+     * @return Returns the response as a serializable object.
+     * @throws DataAccessException If an error occurred when accessing the database.
+     */
+    public FillResponse fill(String userName, int numGenerations) throws DataAccessException {
+        Database db = new Database();
+        int personsAdded = 0;
+        int eventsAdded;
+        try {
+            // Delete all existing events associated with userName
+            EventDAO eDao = new EventDAO(db.openConnection());
+            eDao.deleteByUser(userName);
+            db.closeConnection(true);
 
-			// Get the user's PersonID
-			UserDAO uDao = new UserDAO(db.openConnection());
-			User user = uDao.find(userName);
-			String userPersonID = user.getPersonID();
-			db.closeConnection(true);
+            // Get the user's PersonID
+            UserDAO uDao = new UserDAO(db.openConnection());
+            User user = uDao.find(userName);
+            String userPersonID = user.getPersonID();
+            db.closeConnection(true);
 
-			PersonDAO pDao = new PersonDAO(db.openConnection());
-			ArrayList<Event> eventList = new ArrayList<>();
+            PersonDAO pDao = new PersonDAO(db.openConnection());
 
 			// Get the user's Person object, so we can reinsert after deleting user's data
-			Person userPerson = pDao.find(userPersonID);
-			userPerson.setFatherID(null);
-			userPerson.setMotherID(null);
-			userPerson.setSpouseID(null);
+            Person userPerson = pDao.find(userPersonID);
+            userPerson.setFatherID(null);
+            userPerson.setMotherID(null);
+            userPerson.setSpouseID(null);
 
-			// Generate events for user's person, store for later
-			ArrayList<Event> eventsToAdd = DataGenerator.generateEvents(userPerson);
-			assert eventsToAdd != null;
-			eventList.addAll(eventsToAdd);
+            // Generate events for user's person, store for later
+            ArrayList<Event> eventsToAdd = DataGenerator.generateEvents(userPerson);
+			ArrayList<Event> eventList = new ArrayList<>(eventsToAdd);
 
-			// Delete all existing persons associated with userName
-			pDao.deleteByUser(userName);
+            // Delete all existing persons associated with userName
+            pDao.deleteByUser(userName);
 
-			// Generate indicated number of generations and insert into database
-			ArrayList<Person> genList = new ArrayList<>();
-			genList.add(userPerson);
-			for (int i = 0; i < numGenerations; ++i) {
-				// Initialize next generation
-				ArrayList<Person> nextGenList = new ArrayList<>();
-				for (Person person : genList) {
-					// Generate parents for each person
-					Person[] persons = DataGenerator.generateParents(person);
-					assert persons != null;
+            // Generate indicated number of generations and insert into database
+            ArrayList<Person> genList = new ArrayList<>();
+            genList.add(userPerson);
+            for (int i = 0; i < numGenerations; ++i) {
+                // Initialize next generation
+                ArrayList<Person> nextGenList = new ArrayList<>();
+                for (Person person : genList) {
+                    // Generate parents for each person
+                    Person[] persons = DataGenerator.generateParents(person);
+                    assert persons != null;
 
-					// Link parents to person
-					person.setFatherID(persons[0].getPersonID());
-					person.setMotherID(persons[1].getPersonID());
+                    // Link parents to person
+                    person.setFatherID(persons[0].getPersonID());
+                    person.setMotherID(persons[1].getPersonID());
 
-					// Insert person into database
-					pDao.insert(person);
-					personsAdded++;
+                    // Insert person into database
+                    pDao.insert(person);
+                    personsAdded++;
 
-					// Add parents to next generation.
-					nextGenList.add(persons[0]);
-					nextGenList.add(persons[1]);
+                    // Add parents to next generation.
+                    nextGenList.add(persons[0]);
+                    nextGenList.add(persons[1]);
 
-					try {
-						// Generate events for parents, store for later
-						eventList = DataGenerator.generateParentEvents(eventList, person, persons[0], persons[1]);
-					} catch (FileNotFoundException ex) {
-						ex.printStackTrace();
-					}
-				}
-				// Now that we have finished adding parents for everyone in this generation, move on to next generation
-				genList = nextGenList;
-			}
+                    // Generate events for parents, store for later
+                    eventList = DataGenerator.generateParentEvents(eventList, person, persons[0], persons[1]);
 
-			// Insert the remaining generation into the database
-			for (Person person : genList) {
-				pDao.insert(person);
-				personsAdded++;
-			}
+                }
+                // Now that we have finished adding parents for everyone in this generation, move on to next generation
+                genList = nextGenList;
+            }
 
-			db.closeConnection(true);
+            // Insert the remaining generation into the database
+            for (Person person : genList) {
+                pDao.insert(person);
+                personsAdded++;
+            }
 
-			// Add stored events to database
-			eDao = new EventDAO(db.openConnection());
-			for (Event event : eventList) {
-				eDao.insert(event);
-			}
-			eventsAdded = eventList.size();
+            db.closeConnection(true);
 
-			db.closeConnection(true);
-		} catch (DataAccessException ex) {
-			db.closeConnection(false);
-			throw ex;
-		} catch (NullPointerException ex) {
-			db.closeConnection(false);
-			throw ex;
-		}
+            // Add stored events to database
+            eDao = new EventDAO(db.openConnection());
+            for (Event event : eventList) {
+                eDao.insert(event);
+            }
+            eventsAdded = eventList.size();
+
+            db.closeConnection(true);
+        } catch (DataAccessException | NullPointerException ex) {
+            db.closeConnection(false);
+            throw ex;
+        }
 
 		return new FillResponse("Successfully added " +
-				personsAdded + " persons and " +
-				eventsAdded + " events to the database.", true);
-	}
+                personsAdded + " persons and " +
+                eventsAdded + " events to the database.", true);
+    }
 }
